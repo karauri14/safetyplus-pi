cb764a09b730e20a322e266fd991736a1405a3a6
karauri14 <tamanaha3141@gmail.com>
2018-10-03 17:27:21 +0900

    update logoDisp

--- logoDisp.py
+++ logoDisp.py
@@ -1,13 +1,34 @@
 import cv2
 
+def fadeIn(logo, bg):
+    i = 0
+    while i <= 1:
+        res = cv2.addWeighted(logo, i, bg, 1 - i, 0)
+        i += 0.01
+        cv2.imshow('logo', res)
+        cv2.waitKey(10)
+
+def fadeOut(logo, bg):
+    i = 0
+    while i <= 1:
+        res = cv2.addWeighted(logo, 1 - i, bg, i, 0)
+        i += 0.01
+        cv2.imshow('logo', res)
+        cv2.waitKey(10)
+
 def main():
     cv2.namedWindow('logo', cv2.WINDOW_AUTOSIZE)
     #cv2.namedWindow('logo', cv2.WINDOW_NORMAL)
     #cv2.setWindowProperty('logo', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
     logo = cv2.imread('./img/logo.png')
+    bg = cv2.imread('./img/background.png')
+    
+    fadeIn(logo, bg)
     cv2.imshow('logo', logo)
     
+    cv2.waitKey(3000)
+    fadeOut(logo, bg)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
