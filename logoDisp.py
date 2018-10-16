1213a73a0c25ec989d3f8392d57692af37b4b8b2
karauri14 <tamanaha3141@gmail.com>
2018-10-16 09:19:49 +0900

    update logoDisp

--- logoDisp.py
+++ logoDisp.py
@@ -1,20 +1,23 @@
 import cv2
 
+WAIT_TIME = 1 
+STEP = 0.04
+
 def fadeIn(logo, bg):
     i = 0
     while i <= 1:
         res = cv2.addWeighted(logo, i, bg, 1 - i, 0)
-        i += 0.01
+        i += STEP
         cv2.imshow('logo', res)
-        cv2.waitKey(10)
+        cv2.waitKey(WAIT_TIME)
 
 def fadeOut(logo, bg):
     i = 0
     while i <= 1:
         res = cv2.addWeighted(logo, 1 - i, bg, i, 0)
-        i += 0.01
+        i += STEP
         cv2.imshow('logo', res)
-        cv2.waitKey(10)
+        cv2.waitKey(WAIT_TIME)
 
 def main():
     #cv2.namedWindow('logo', cv2.WINDOW_AUTOSIZE)
@@ -34,7 +37,7 @@ def main():
     fadeOut(logo, bg)
     
     cv2.imshow('logo', bg)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
     
     cv2.destroyAllWindows()
 
