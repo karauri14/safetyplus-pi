c80b1802757b2a827775e9aa6cf7e2484ade1129
karauri14 <tamanaha3141@gmail.com>
2018-12-20 11:47:34 +0900

    refactoring

--- signListener.py
+++ signListener.py
@@ -20,12 +20,10 @@ OVER_TIME = 10
 STOP_TIME = 5
 SLOW_TIME = 30
 
-SHRINK = 1.0
-
-lower_red1 = np.array([0,10,20])
-upper_red1 = np.array([20,255,255])
-lower_red2 = np.array([160,10,20])
-upper_red2 = np.array([180,255,255])
+LOWER_RED1 = np.array([0,10,20])
+UPPER_RED1 = np.array([20,255,255])
+LOWER_RED2 = np.array([160,10,20])
+UPPER_RED2 = np.array([180,255,255])
     
 ops.reset_default_graph()
 sess = tf.Session()
@@ -50,92 +48,74 @@ def classification(ROI):
     
     pred = sess.run(GRAPH_NAME['prediction'], feed_dict={GRAPH_NAME['input_image']:ROI_arr, GRAPH_NAME['keep_prob']:1.0})
     label = np.argmax(pred, 1)
-    predict_label = SIGN[int(label)]
-    #print (predict_label)
-    return(predict_label)
+    predictLabel = SIGN[int(label)]
+    
+    return(predictLabel)
 
-def find_contour_using_red_filter(src):
-    red_segment = red_mask(src)
-    image, contours, hierarchy = cv2.findContours(red_segment, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    #cv2.imshow('red', red_segment)
+def findContourUsingRedFilter(src):
+    redSegment = redMask(src)
+    image, contours, hierarchy = cv2.findContours(redSegment, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    
     return contours
 
-def red_mask(src):
+def redMask(src):
     hsv = cv2.cvtColor(src,cv2.COLOR_BGR2HSV)
-    mask1 = cv2.inRange(hsv,lower_red1,upper_red1)
-    mask2 = cv2.inRange(hsv,lower_red2,upper_red2)
+    mask1 = cv2.inRange(hsv,LOWER_RED1,UPPER_RED1)
+    mask2 = cv2.inRange(hsv,LOWER_RED2,UPPER_RED2)
 
     mask = cv2.bitwise_or(mask1, mask2)
 
-    mask_not = cv2.bitwise_not(mask)
+    maskNot = cv2.bitwise_not(mask)
 
-    return mask_not
+    return maskNot
 
-def detect_contour(src, sign_count):
+def detectContour(src, signCount):
 
-    ##method: using only red filter
-    contours = find_contour_using_red_filter(src)
+    contours = findContourUsingRedFilter(src)
     
-    #1st condition
     for cnt in contours:
         #area(float) of each countour(list)
         area = cv2.contourArea(cnt)
 
-        #if the size of area less than 500 || more than 3000, just ignore
+        #if the size of area less than 100 || more than 4000, just ignore
         if area < MIN_AREA or MAX_AREA < area:
           continue
+        
+        #create bounding box(x,y,w,h) around countours
+        x, y, w, h = cv2.boundingRect(cnt)
+        ratio = float(h) / float(w)
 
-        #if it is between 500 and 3000, create bounding box(x,y,w,h) around countours
-        if len(cnt) > 0:
-          rect = cnt
-          x, y, w, h = cv2.boundingRect(rect)
-          
-        if (float(h) / float(w)) < MAX_RATIO and (float(h) / float(w)) > MIN_RATIO:
+        if ratio < MAX_RATIO and ratio > MIN_RATIO:
             ROI = src[y:y + h, x:x + w]
             
             #"Takeover Sign", "Negative","Slow Sign", "Stop Sign"
             sign = classification(ROI)
             if sign == 'Takeover Sign':
-                sign_count['OVER'] = OVER_TIME
+                signCount['OVER'] = OVER_TIME
             elif sign == 'Slow Sign':
-                sign_count['SLOW'] = SLOW_TIME
+                signCount['SLOW'] = SLOW_TIME
             elif sign == 'Stop Sign':
-                sign_count['STOP'] = STOP_TIME
-    
-    #kishimon
-    #if laneOver.isNotOver(src) == True:
-    #    sign_count['OVER'] = OVER_TIME
-    
-    return (make_state_string(sign_count))
+                signCount['STOP'] = STOP_TIME
+      
+    return (makeStateString(signCount))
 
-def make_state_string(sign_count):
+def makeStateString(signCount):
     
-    string = ""
-    
-    if sign_count['STOP'] != 0:
-        sign_count['STOP'] -= 1
-        string += "stop,"
-    else :
-        string += ","
-        
-    if sign_count['SLOW'] != 0:
-        sign_count['SLOW'] -= 1
-        string += "slow,"
-    else :
-        string += ","
-        
-    if sign_count['OVER'] != 0:
-        sign_count['OVER'] -= 1
-        string += "over"
-    else :
-        string += ""
+    string = ''
+
+    for key in signCount:
+        if signCount[key] != 0:
+            signCount[key] -= 1
+            string += (key.lower() + ',')
+        else :
+            string += ','
+
+    string = string[:-1]
     
     return (string)
 
-def listener(video, sign_count):
-    is_read, frame = video.read()
-    frame = cv2.resize(frame, None, fx = SHRINK, fy = SHRINK, interpolation = cv2.INTER_LINEAR)
+def listener(video, signCount):
+    isRead, frame = video.read()
     
-    #cv2.imshow('camera', frame)
     if frame is not None:
-        return (detect_contour(frame, sign_count))
+        return (detectContour(frame, signCount))
